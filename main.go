79a0b6c69adc688514f77533380e429ce88632d7
shyam <shyam10kwd@gmail.com>
2025-12-03 17:16:16 +0530

    new binaries

--- main.go
+++ main.go
@@ -3,69 +3,55 @@ package main
 //let's start
 
 import (
+	// "database/sql"
+	"chirpy/internal/database"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"sync/atomic"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-type message struct {
-	text         string
-	content_type string
-}
-
-func (m message) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", m.content_type)
-	w.WriteHeader(200)
-	fmt.Fprint(w, m.text)
-}
-
-func (cfg *apiConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %v times!</p>\n</body>\n</html>", cfg.fileserverHits.Load())
-}
-
-func (cfg *apiConfig) middleWareInc(handle http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		handle.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) middleWareReset(handle http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Store(0)
-		handle.ServeHTTP(w, r)
-	})
-}
 
 const port = "8080"
 
 func main() {
 
-	cfg := apiConfig{}
-	m := message{text: "OK", content_type: "text/plain; charset=utf-8"}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
 
-	fmt.Println("Server listening at ")
+	cfg := apiConfig{}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error opening the databse from url")
+		return
+	}
+
+	cfg.role = os.Getenv("PLATFORM")
+	dbQueries := database.New(db)
+	cfg.db = *dbQueries
+
+	fmt.Println("Server listening at ", port)
 	serverMux := http.NewServeMux()
 	filepathRoot := ""
-	// serverMux.HandleFunc("/healthz", m.ServeHttp)
-	serverMux.Handle("GET /api/healthz", m)
+	serverMux.Handle("POST /api/users", http.HandlerFunc(cfg.CreatUserHandler))
+	serverMux.Handle("POST /api/chirps", http.HandlerFunc(cfg.CreatChirpHandler))
 	serverMux.Handle("GET /admin/metrics", &cfg)
 	serverMux.Handle("/app/", cfg.middleWareInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	serverMux.Handle("POST /admin/reset", cfg.middleWareReset(m))
+	serverMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.ResetHandler))
 	serverMux.Handle("/app/assets/logo.png", cfg.middleWareInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	localServer := &http.Server{
 		Addr:    ":" + port,
 		Handler: serverMux,
 	}
-	err := localServer.ListenAndServe()
+	err = localServer.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
